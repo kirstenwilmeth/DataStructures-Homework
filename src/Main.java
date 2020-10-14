1de8fbb80bd9b6bdffeb7fc95189a2a2e3dadb79
Kirsten <>
2020-10-14 15:15:31 -0500

    Default Files

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+    HomeWork2 home = new HomeWork2();
+    
+    home.run();
+    
+    // ArrayL myList = new ArrayL();   
+
+    // myList.run();
+
+    // myHashMap hash = new myHashMap();
+
+    // hash.run();
+   }
+}
