e0facf40d9ca5d47d36e69bd657046330438b5f9
Kirsten <>
2020-10-14 15:15:16 -0500

    Homework Two

---
+++ src/HomeWork2.java
@@ -0,0 +1,33 @@
+
+
+public class HomeWork2 {
+    
+    public void run(){
+   
+    int[] numbers = {3, 7, 13, 11, 9};
+   
+    double sum = 0;
+        for (int i : numbers) {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+
+        // Answer to Question 2 = 2?
+
+
+
+}
+
+public static void toPower(String [] args){
+    int size;
+    int power;
+
+// int[] numbers = {1,5,6,5,4,1};
+
+    size = 4;
+    power = 2;
+    
+    
+}
+
+}
---
+++ src/toPower.java
@@ -0,0 +1,16 @@
+
+public class toPower {
+    
+    public static void toPower(String [] args){
+        int size;
+        int power;
+
+// int[] numbers = {1,5,6,5,4,1};
+
+        size = 4;
+        power = 2;
+        
+        
+    }
+
+}
